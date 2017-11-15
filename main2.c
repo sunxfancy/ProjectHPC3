268193011906b96e74fd1cf146c3f0901628a867
西风逍遥游 <sun_xfancy@163.com>
2017-11-15 15:55:13 -0800

    final fix

--- main2.c
+++ main2.c
@@ -36,9 +36,9 @@ int main(int argc, char *argv[])
         exit(1);
     }
     n = atoll(argv[1]);
-    low_value = 3 + BLOCK_LOW(id, p, n - 1);
-    high_value = 3 + BLOCK_HIGH(id, p, n - 1);
-    size = BLOCK_SIZE(id, p, n - 1);
+    low_value = 3 + 2*BLOCK_LOW(id, p, (n - 1)/2);
+    high_value = 3 + 2*BLOCK_HIGH(id, p, (n - 1)/2);
+    size = 2*BLOCK_SIZE(id, p, (n - 1)/2);
     proc0_size = (n - 1) / p;
     sqrtn = (long long)sqrt((double)n);
     if ((3 + proc0_size) < sqrtn)
