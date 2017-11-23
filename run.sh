0d30f75e6ff1c95b90e1bd18c94e8a0b0a898fa6
西风逍遥游 <sun_xfancy@163.com>
2017-11-23 15:03:58 -0800

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+make clean
 jobid=$(qsub jobfile | cut -d '.' -f 1)
 jobid=$(qsub -W depend=afterok:${jobid} jobfile2 | cut -d '.' -f 1)
 jobid=$(qsub -W depend=afterok:${jobid} jobfile4 | cut -d '.' -f 1)
